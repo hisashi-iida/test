ae2be379664826a36abf156c3040b7a0a456b2c1
飯田尚 <iidahisashi@iidanaonoMacBook.local>
2018-05-01 09:21:28 +0900

    change using key in external file

--- voicetext/test.py
+++ voicetext/test.py
@@ -2,7 +2,7 @@
 
 from pyVoiceText import VoiceText
 
-mykey='emwix5ragfw1sux7'
+mykey=open('./key.txt', 'r').read(16)
 vt = VoiceText(mykey)
 vt.fetch(text='こんにちは世界', speaker='hikari', emotion_level=1, pitch=100, speed=300, volume=100)
 
