515dcaf48b4350d28904f03cce7f1a05f3ed789c
飯田尚 <iidahisashi@iidanaonoMacBook.local>
2018-05-04 09:44:16 +0900

    hello world

--- voicetext/test.py
+++ voicetext/test.py
@@ -2,7 +2,20 @@
 
 from pyVoiceText import VoiceText
 
-mykey=open('./key.txt', 'r').read(16)
-vt = VoiceText(mykey)
-vt.fetch(text='こんにちは世界', speaker='hikari', emotion_level=1, pitch=100, speed=300, volume=100)
+def voicetext(mykey, text, outfile, speed):
+    vt = VoiceText(mykey)
+    wave = vt.fetch(
+        text=text,
+        speaker='hikari',
+        emotion_level=1,
+        pitch=100,
+        speed=speed,
+        volume=100)
+    vt.save(wave, outfile)
+
+
+if __name__ == "__main__":
+    mykey=open('./key.txt', 'r').read(16)
+    voicetext(mykey, 'Hello World', 'out1.wav', 100)
+    voicetext(mykey, 'こんにちは世界', 'out2.wav', 300)
 
