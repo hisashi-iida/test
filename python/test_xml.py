d5d6afa62c840d29d55292112587106da889b516
飯田尚 <iidahisashi@iidanaonoMacBook.local>
2018-05-07 05:30:52 +0900

    modify for metadata.opf

--- python/test_xml.py
+++ python/test_xml.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 import xml.etree.ElementTree as et
+import sys
 
-tree = et.ElementTree(file='test.xml')
-root = tree.getroot()
-print(root.tag)
-for child in root:
-    print('tag:', child.tag, 'attributes:', child.attrib)
-    for grandchild in child:
-        print('    tag:', grandchild.tag)
-        print('    attributes:', grandchild.attrib)
-        print('    text:', grandchild.text)
+def xmlparse(fn):
+    tree = et.ElementTree(file=fn)
+    root = tree.getroot()
+    for child in root:
+        for grandchild in child:
+            if grandchild.tag == '{http://purl.org/dc/elements/1.1/}title':
+                print(grandchild.text)
+        for grandchild in child:
+            if grandchild.tag == '{http://purl.org/dc/elements/1.1/}creator':
+                print(grandchild.text)
+
+
+if __name__ == "__main__":
+    a = sys.argv
+    xmlparse(a[1])
 
 
