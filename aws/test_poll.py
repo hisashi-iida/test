cd3ff15f271fc7672033c39dbe8fd7e0b3df938f
飯田尚 <iidahisashi@MacBook.local>
2018-05-17 09:44:06 +0900

    now it works

--- aws/test_poll.py
+++ aws/test_poll.py
@@ -6,9 +6,12 @@ client = boto3.client('polly')
 
 response = client.synthesize_speech(
     OutputFormat='mp3',
-    SpeechMarkTypes='sentence',
     Text='こんにちは世界。私はポリーです。私の日本語はいかがでしょうか？',
     TextType='text',
     VoiceId='Mizuki'
 #    VoiceId='Takumi'
 )
+print(response)
+#print(response['AudioStream'])
+open('output.mp3', 'wb').write(response['AudioStream'].read())
+
