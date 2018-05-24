7e20b613774b162ee971dd4e3f36d26362326ab8
飯田尚 <iidahisashi@MacBook.local>
2018-05-24 09:11:20 +0900

    bugfix: wavfile -> wavfiles

--- play_scenario.sh
+++ play_scenario.sh
@@ -44,7 +44,7 @@ while true; do
     sleep 0.1
 done
 sleep 0.1
-wavfile=""
+wavfiles=""
 for f in $(cat mp3files_list.txt); do
     ffmpeg -i ${f} -ar 44100 ${f}.wav > /dev/null 2>&1
     wavfiles="${wavfiles} ${f}.wav"
