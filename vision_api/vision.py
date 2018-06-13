e3c838e5fdfd7fdd2845390df05a28e891e920e9
master <username@example.com>
2018-06-13 09:25:42 +0900

    change it detecting text

--- vision_api/vision.py
+++ vision_api/vision.py
@@ -24,11 +24,11 @@ with io.open(file_name, 'rb') as image_file:
 
     image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+# Performs text detection on the image file
+response = client.text_detection(image=image)
+texts = response.text_annotations
 
 print('Labels:')
-for label in labels:
-        print(label.description)
+for text in texts:
+        print(text.description)
 
