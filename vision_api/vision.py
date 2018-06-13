538131831e3248df2bea3e26f18d4ab932babc6a
master <username@example.com>
2018-06-13 21:37:51 +0900

    simplify

--- vision_api/vision.py
+++ vision_api/vision.py
@@ -1,34 +1,14 @@
 #!/usr/bin/env python3
-import io
-import os
 
-# Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
-
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
-
-# The name of the image file to annotate
-#file_name = os.path.join(
-#    os.path.dirname(__file__),
-#        'resources/wakeupcat.jpg')
-
 import sys
-argv = sys.argv
-file_name = argv[1];
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-    image = types.Image(content=content)
-
-# Performs text detection on the image file
-response = client.text_detection(image=image)
-texts = response.text_annotations
-
-print('Labels:')
+content = open(sys.argv[1], 'rb').read()
+image = types.Image(content=content)
+client = vision.ImageAnnotatorClient()
+result = client.text_detection(image=image)
+texts = result.text_annotations
 for text in texts:
-        print(text.description)
+    print(text.description)
 
